710b2b1e62daade86a25795177718229bb891544
dleooo <dleo@torontomu.ca>
2024-10-28 21:48:50 -0400

    .

---
+++ form.js
@@ -0,0 +1,51 @@
+// stores firstname
+localStorage.setItem("fname", value ); 
+console.log(localStorage.getItem("fname"));
+
+// stores lastname
+localStorage.setItem("lname", value ); 
+console.log(localStorage.getItem("lname"));
+
+// stores email
+localStorage.setItem("email", value ); 
+console.log(localStorage.getItem("email"));
+
+// stores SIN
+localStorage.setItem("sin", value ); 
+console.log(localStorage.getItem("sin"));
+
+// stores question 1 - Interactive elements
+localStorage.setItem("choice1", value ); 
+console.log(localStorage.getItem("choice1"));
+
+// stores question 1 - Clean and organized layout
+localStorage.setItem("choice2", value ); 
+console.log(localStorage.getItem("choice2"));
+
+// stores question 1 - Easy navigation
+localStorage.setItem("choice4", value ); 
+console.log(localStorage.getItem("choice3"));
+
+// stores question 1 - Contact information and accessibility
+localStorage.setItem("choice4", value ); 
+console.log(localStorage.getItem("choice4"));
+
+// stores question 2 - yes
+localStorage.setItem("yes", value ); 
+console.log(localStorage.getItem("yes"));
+
+// stores question 2 - no
+localStorage.setItem("no", value ); 
+console.log(localStorage.getItem("no"));
+
+// stores question 3
+localStorage.setItem("longAnswer", value ); 
+console.log(localStorage.getItem("longAnswer"));
+
+// stores question 4
+localStorage.setItem("colours", value ); 
+console.log(localStorage.getItem("colours"));
+
+// stores question 5
+localStorage.setItem("ages", value ); 
+console.log(localStorage.getItem("ages"));
